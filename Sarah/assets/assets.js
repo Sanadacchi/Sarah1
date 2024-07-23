fae77537bfaaf27bd6890c4d7ddd1f6599531a4e
Sanadacchi <akisd47@gmail.com>
2024-07-23 21:54:55 +0100

    Add files via upload

---
+++ Sarah/assets/assets.js
@@ -0,0 +1,376 @@
+const products = [
+{    id : '1',
+    image : 'assets/cloverNecklaces.png',
+    name : 'Butterfly Necklace',
+    priceNaira:'Free',
+     itemType:'necklace'
+},
+{    id : '2',
+    image : 'assets/earrings1.png',
+    name : 'Butterfly Necklace set',
+    priceNaira:'Free',
+     itemType:'necklace'
+},
+{    id : '3',
+    name: 'hairband',
+    image : 'assets/hairBand1.png',
+    priceNaira:'Free',
+     itemType:'Bracelets'
+},
+{    id : '4',
+    name: 'hairband',
+    image : 'assets/hairBand2.png',
+    priceNaira:'Free',
+     itemType:'necklace'
+},
+{    id : '5',
+    image:'assets/hairBand3.png',
+    name : 'Butterfly Necklace',
+    priceNaira:'Free',
+     itemType:'necklace'
+},
+{    id : '6',
+    image:'assets/butterfly1.png',
+    name : 'Butterfly Necklace',
+    priceNaira:'Free',
+     itemType:'necklace'
+},
+{    id : '7',
+    image:'assets/necklace1.png',
+    name : 'Butterfly Necklace',
+    priceNaira:'Free',
+     itemType:'necklace'
+},
+{    id : '8',
+    image:'assets/butterfly2.png',
+    name : 'Necklaces',
+    priceNaira:'Free',
+     itemType:'necklace'
+},
+   
+];
+
+const preOrders = [
+    {
+       id : '11',
+        image : 'assets/batman-rings.jpg',
+        name : 'Batman rings',
+        priceNaira:'Free',
+         itemType:'Ring'
+},
+{
+       id : '12',
+        image : 'assets/black-butterflies.jpg',
+        name : 'Black Butterflies',
+        priceNaira:'Free',
+         itemType:'Necklace earring'
+},
+{
+       id : '13',
+        image : 'assets/blue-bracelets.jpg',
+        name : 'Blue Bracelets',
+        priceNaira:'Free',
+         itemType:'Bracelets'
+},
+{
+       id : '14',
+        image : 'assets/bracelets.png',
+        name : 'Bracelet',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '15',
+        image : 'assets/butterfly-set.jpg',
+        name : 'Butterfly Set',
+        priceNaira:'Free',
+         itemType:'Butterfly Set'
+},
+{
+       id : '16',
+        image : 'assets/butterfly-stuff.jpg',
+        name : 'Butterfly Stuff',
+        priceNaira:'Free',
+         itemType:'Butterfly Stuff'
+},
+{
+       id : '17',
+        image : 'assets/clip-thingies.jpg',
+        name : 'Clip Thingy',
+        priceNaira:'Free',
+         itemType:'Clip'
+},
+{
+       id : '18',
+        image : 'assets/colourful-bracelets.jpg',
+        name : 'Multi-coloured bracelets',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '19',
+        image : 'assets/colourful-necklaces.jpg',
+        name : 'Multi-coloured Necklaces',
+        priceNaira:'Free',
+         itemType:'Necklace'
+},
+{
+       id : '20',
+        image : 'assets/earrings2.png',
+        name : 'Earrings',
+        priceNaira:'Free',
+         itemType:'Earrings'
+},
+{
+       id : '21',
+        image : 'assets/gold-batman-rings.jpg',
+        name : 'Gold batman rings',
+        priceNaira:'Free',
+         itemType:'Ring'
+},
+{
+       id : '22',
+        image : 'assets/gold-butterflies1.jpg',
+        name : 'Gold Butterflies',
+        priceNaira:'Free',
+         itemType:'Necklace earrings'
+},
+{
+       id : '23',
+        image : 'assets/gold-set1.jpg',
+        name : 'Gold Set',
+        priceNaira:'Free',
+         itemType:'Necklace earrings'
+},
+{
+       id : '24',
+        image : 'assets/gold-set1.jpg',
+        name : 'Gold Set',
+        priceNaira:'Free',
+         itemType:'Necklace earrings'
+},
+{
+       id : '25',
+        image : 'assets/gold-stuff.jpg',
+        name : 'Gold Stuff',
+        priceNaira:'Free',
+         itemType:'Earrings'
+},
+{
+       id : '26',
+        image : 'assets/golden-butterly.jpg',
+        name : 'Golden Butterfly',
+        priceNaira:'Free',
+         itemType:'Necklace Earrings'
+},
+{
+       id : '27',
+        image : 'assets/green-bracelet.jpg',
+        name : 'Green Bracelet',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '28',
+        image : 'assets/green-butterflies.jpg',
+        name : 'Green Butterflies',
+        priceNaira:'Free',
+         itemType:'Necklace earrings'
+},
+{
+       id : '29',
+        image : 'assets/hair-clips.jpg',
+        name : 'Hair Clips',
+        priceNaira:'Free',
+         itemType:'Hair'
+},
+{
+       id : '30',
+        image : 'assets/hair-clips1.jpg',
+        name : 'Hair Clips',
+        priceNaira:'Free',
+         itemType:'Hair'
+},
+{
+       id : '31',
+        image : 'assets/hair-stuff-sigh.jpg',
+        name : 'Hair Stuff',
+        priceNaira:'Free',
+         itemType:'Hair'
+},
+{
+       id : '32',
+        image : 'assets/heart-necklace.jpg',
+        name : 'Heart Necklace',
+        priceNaira:'Free',
+         itemType:'Necklace'
+},
+{
+       id : '33',
+        image : 'assets/heart-necklace.jpg',
+        name : 'Heart Necklace',
+        priceNaira:'Free',
+         itemType:'Necklace'
+},
+{
+       id : '34',
+        image : 'assets/Ionno mehn.jpg',
+        name : 'Earrings',
+        priceNaira:'Free',
+         itemType:'earrings'
+},
+{
+       id : '35',
+        image : 'assets/multi-bracelet.jpg',
+        name : 'Multi-Coloured Bracelet',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '36',
+        image : 'assets/necklace1.png',
+        name : 'Bracelets',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '37',
+        image : 'assets/nice-bracelet.jpg',
+        name : 'Bracelets',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '38',
+        image : 'assets/nice-bracelet1.jpg',
+        name : 'Bracelets',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '39',
+        image : 'assets/nice-bracelet2.jpg',
+        name : 'Bracelets',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '40',
+        image : 'assets/phone-case.jpg',
+        name : 'Iphone Case',
+        priceNaira:'Free',
+         itemType:'Phone Case'
+},
+{
+       id : '41',
+        image : 'assets/pink-bracelets.jpg',
+        name : 'Bracelets',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '42',
+        image : 'assets/samsung-case.jpg',
+        name : 'Bracelets',
+        priceNaira:'Free',
+         itemType:'Phone Case'
+},
+{
+       id : '43',
+        image : 'assets/samsung-case.jpg',
+        name : 'Samsung Phone Case',
+        priceNaira:'Free',
+         itemType:'Phone Case'
+},
+{
+       id : '44',
+        image : 'assets/samsung-case.jpg',
+        name : 'Samsung Phone Case',
+        priceNaira:'Free',
+         itemType:'Phone Case'
+},
+{
+       id : '45',
+        image : 'assets/set1.jpg',
+        name : 'Jewellery Set',
+        priceNaira:'Free',
+         itemType:'Necklace earrings'
+},
+{
+       id : '46',
+        image : 'assets/set4.jpg',
+        name : 'Jewellery Set',
+        priceNaira:'Free',
+         itemType:'Necklace earrings'
+},
+{
+       id : '47',
+        image : 'assets/set5.jpg',
+        name : 'Jewellery Set',
+        priceNaira:'Free',
+         itemType:'Necklace earrings'
+},
+{
+       id : '48',
+        image : 'assets/silver-heart.jpg',
+        name : 'Jewellery Set',
+        priceNaira:'Free',
+         itemType:'Necklace earrings'
+},
+{
+       id : '49',
+        image : 'assets/3butterly-stuff.jpg',
+        name : ' Butterfly Jewellery',
+        priceNaira:'Free',
+         itemType:'Necklace'
+},
+{
+       id : '50',
+        image : 'assets/thingies.jpg',
+        name : 'Something',
+        priceNaira:'Free',
+         itemType:'Stuff'
+},
+{
+       id : '51',
+        image : 'assets/white-bracelets.jpg',
+        name : 'White Bracelet',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '52',
+        image : 'assets/white-bracelets.jpg',
+        name : 'White Bracelet',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '53',
+        image : 'assets/white-bracelets2.jpg',
+        name : 'White Bracelet',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '54',
+        image : 'assets/yin-yang-set.jpg',
+        name : 'Yin Yang Jewellery',
+        priceNaira:'Free',
+         itemType:'Necklace earrings'
+},
+{
+       id : '55',
+        image : 'assets/Yin-yang-watch.jpg',
+        name : 'Yin Yang Watch',
+        priceNaira:'Free',
+         itemType:'Bracelet'
+},
+{
+       id : '56',
+        image : 'assets/yinYang.jpg',
+        name : 'Yin Yang Jewellry',
+        priceNaira:'Free',
+         itemType:'Necklace earrings'
+},
+];
